5cdf91e0afc68c9eb0e07e08679c32603008dc5c
MrDG5 <mentallimd@gmail.com>
2021-05-29 19:44:40 +0300

    experimenting in promotion of types

--- STP_01_HW/main.cpp
+++ STP_01_HW/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+int compare(int i, unsigned to)
+{
+	return i >= to;
+}
+
 int main()
 {
 	std::cout << "Hello, World!\n";
-	return 0;
+	
+	unsigned from = 100;
+	unsigned to = 0;
+	for (int i = from; compare(i, to); --i) 
+	{
+		i = -1;
+	}
+
+
+ 	return 0;
 }
